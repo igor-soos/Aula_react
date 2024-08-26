240bf95ab5de8370126c13680e711a16a3419acc
igor <igorsoosrribeiro@gmail.com>
2024-08-26 11:25:39 -0300

    criando caminhos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Home from './routes/Home.jsx'
+import Aluno from './routes/Aluno.jsx'
+
+const router =createBrowserRouter([
+  
+  {
+    //chamando elemento pai
+    path:'/', element:<app/>,
+    errorElement:<error/>,
+
+   //chamando elemento filho
+    children:[
+      {path:'/',element:<Home/>},
+      {path:'/',element:<Aluno/>},
+    ]
+  }
+])
 
 
 createRoot(document.getElementById('root')).render(
